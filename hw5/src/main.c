c1cae8187b1b8522230d3fa26c4a163f6bafaef8
David Park <dappark@cs.stonybrook.edu>
2022-04-21 14:51:43 -0400

    Basic main() implementation

--- hw5/src/main.c
+++ hw5/src/main.c
@@ -1,5 +1,10 @@
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <pthread.h>
 
 #include "pbx.h"
 #include "server.h"
@@ -12,11 +17,16 @@ static void terminate(int status);
  *
  * Usage: pbx <port>
  */
-int main(int argc, char* argv[]){
+int main(int argc, char *argv[])
+{
     // Option processing should be performed here.
     // Option '-p <port>' is required in order to specify the port number
     // on which the server should listen.
-
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s -p <port>\n", argv[0]);
+        terminate(EXIT_FAILURE);
+    }
     // Perform required initialization of the PBX module.
     debug("Initializing PBX...");
     pbx = pbx_init();
@@ -26,19 +36,69 @@ int main(int argc, char* argv[]){
     // run function pbx_client_service().  In addition, you should install
     // a SIGHUP handler, so that receipt of SIGHUP will perform a clean
     // shutdown of the server.
+    char *port = argv[2];
+    struct addrinfo hints, *listp, *p;
+    int listenfd, *connfd, optval = 1;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
 
-    fprintf(stderr, "You have to finish implementing main() "
-	    "before the PBX server will function.\n");
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_socktype = SOCK_STREAM;             /* Accept connections */
+    hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG; /* ... on any IP address */
+    hints.ai_flags |= AI_NUMERICSERV;            /* ... using port number */
+    getaddrinfo(NULL, port, &hints, &listp);
 
-    terminate(EXIT_FAILURE);
+    /* Walk the list for one that we can bind to */
+    for (p = listp; p; p = p->ai_next)
+    {
+        /* Create a socket descriptor */
+        if ((listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
+            continue; /* Socket failed, try the next */
+
+        /* Eliminates "Address already in use" error from bind */
+        setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+                   (const void *)&optval, sizeof(int));
+
+        /* Bind the descriptor to the address */
+        if (bind(listenfd, p->ai_addr, p->ai_addrlen) == 0)
+            break;       /* Success */
+        close(listenfd); /* Bind failed, try the next */
+    }
+
+    /* Clean up */
+    freeaddrinfo(listp);
+    if (!p)
+    {
+        fprintf(stderr, "bind: Permission denied\n");
+        terminate(EXIT_FAILURE);
+    }
+
+    if (listen(listenfd, 1024) < 0)
+    {
+        fprintf(stderr, "listen: Permission denied\n");
+        close(listenfd);
+        terminate(EXIT_FAILURE);
+    }
+    while (1)
+    {
+        clientlen = sizeof(struct sockaddr_storage);
+        connfd = malloc(sizeof(int));
+        *connfd = accept(listenfd, (struct sockaddr *)&clientaddr, &clientlen);
+        pthread_create(&tid, NULL, pbx_client_service, connfd);
+    }
+
+    terminate(EXIT_SUCCESS);
 }
 
 /*
  * Function called to cleanly shut down the server.
  */
-static void terminate(int status) {
+static void terminate(int status)
+{
     debug("Shutting down PBX...");
-    pbx_shutdown(pbx);
+    if (pbx)
+        pbx_shutdown(pbx);
     debug("PBX server terminating");
     exit(status);
 }
